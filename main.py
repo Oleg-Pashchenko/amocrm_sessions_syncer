4ba3db51fcf02683c8abbd26416d113ddd780029
olehandro <odpash.itmo@gmail.com>
2024-03-04 11:17:04 +0300

    fixed

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import requests
 import database
 
 
-
 def job():
     accounts = database.read_accounts()
     for account in accounts:
+        if account.host != 'https://saleschef.amocrm.ru/':
+            continue
         try:
             session = requests.post(
                 url='http://amocrm.avatarex.tech/create-tokens/',
                 json={
-                    'amo_host': account.host,
-                    'amo_email': account.email,
-                    'amo_password': account.password
+                    'amo_host': account.host.strip(),
+                    'amo_email': account.email.strip(),
+                    'amo_password': account.password.strip()
                 }).json()
             database.update_session(account, session['answer'])
         except Exception as e:
@@ -24,6 +25,7 @@ def job():
 
 schedule.every(10).minutes.do(job)
 
+print('completed')
 while True:
     schedule.run_pending()
     time.sleep(1)
